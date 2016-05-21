e32c8ca512b20512e6cd3fed64af715ab2314f5c
Bangyou Zheng <zheng.bangyou@gmail.com>
2016-05-21 14:52:03 +1000

    Update _build.R

--- _build.R
+++ _build.R
@@ -2,7 +2,5 @@ args <- commandArgs(TRUE)
 
 token <- args[[1]]
 secret <- args[[2]]
-print(token)
-print(secret)
 rsconnect::setAccountInfo(name='bzheng', token=token, secret=secret)
 rsconnect::deployApp(quiet=TRUE)
