658bdb26703c86e70255850f6ca7eeae0add4e76
Bangyou Zheng <zheng.bangyou@gmail.com>
2016-05-21 15:13:29 +1000

    Delete _build.sh

--- _build.sh
+++
@@ -1,9 +0,0 @@
-set -e
-# configure your name and email if you have not done so
-git config --global user.email "zheng.bangyou@gmail.com"
-git config --global user.name "Bangyou Zheng"
-git clone --branch=master \
-  https://github.com/${TRAVIS_REPO_SLUG}.git \
-  shiny
-cd shiny
-Rscript Rscript _build.R ${SHINY_TOKEN}$ ${SHINY_SECRET}$
