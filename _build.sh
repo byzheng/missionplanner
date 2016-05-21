f87e99badff72f0bd1a9ccf37c56317b435e1390
Bangyou Zheng <zheng.bangyou@gmail.com>
2016-05-21 13:54:48 +1000

    Update _build.sh

--- _build.sh
+++ _build.sh
@@ -2,7 +2,7 @@ set -e
 # configure your name and email if you have not done so
 git config --global user.email "zheng.bangyou@gmail.com"
 git config --global user.name "Bangyou Zheng"
-git clone --branch=gh-pages \
+git clone --branch=master \
   https://github.com/${TRAVIS_REPO_SLUG}.git \
   shiny
 cd shiny
