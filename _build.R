3f046c65ce29c4598279356a22ea5b6455f042b8
zhe00a-vc <Bangyou.Zheng@csiro.au>
2016-05-22 19:52:42 +1000

    Compile Rmd into md.

--- _build.R
+++ _build.R
@@ -2,5 +2,7 @@ args <- commandArgs(TRUE)
 
 token <- args[[1]]
 secret <- args[[2]]
+rmarkdown::render('help.Rmd')
+file.remove('help.html')
 rsconnect::setAccountInfo(name='byzheng', token=token, secret=secret)
 rsconnect::deployApp(quiet=TRUE)
