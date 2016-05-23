c11610926960b9f4beb47aaa79edc460a9fe2d36
zhe00a-vc <Bangyou.Zheng@csiro.au>
2016-05-23 16:31:25 +1000

    Update again.

--- _build.R
+++ _build.R
@@ -4,5 +4,5 @@ token <- args[[1]]
 secret <- args[[2]]
 rmarkdown::render('help.Rmd')
 file.remove('help.html')
-#rsconnect::setAccountInfo(name='byzheng', token=token, secret=secret)
-#rsconnect::deployApp(quiet=TRUE)
+rsconnect::setAccountInfo(name='byzheng', token=token, secret=secret)
+rsconnect::deployApp(quiet=TRUE)
