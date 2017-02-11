2ff0566fa3549ef83e5f9d1d52583965658d0e5a
zhe00a-vc <Bangyou.Zheng@csiro.au>
2017-02-11 13:14:01 +1000

    Rearrange the interface

--- ui.R
+++ ui.R
@@ -10,45 +10,47 @@ ui_configuration <- function() {
     tabsetPanel(
         tabPanel(
             title = 'UAV'
-            , numericInput(
-                inputId = 'i_flight_height'
-                , label = 'Flight height (m)' 
-                , value = 20, min = 5, max = 200, step = 1)
-            
-            , numericInput(
+            , fluidRow(
+                column(width = 6, numericInput(
+                           inputId = 'i_flight_height'
+                           , label = 'Flight height (m)' 
+                           , value = 20, min = 5, max = 200, step = 1))
+            , column(width = 6, numericInput(
                 inputId = 'i_flight_speed'
                 , label = 'Flight speed'
                 , value = 36
                 , min = 1
                 , max = 30
                 #, step = 1
-            )
-            , radioButtons(
+            )))
+            , fluidRow(
+                column(width = 6, radioButtons(
                 inputId = 'i_speed_unit'
                 , label = 'Unit'
                 , choices = c('m/s', 'km/h')
                 , selected = 'm/s'
                 , inline = TRUE
-            )
-            , radioButtons(
+            ))
+            , column(width = 6, radioButtons(
               inputId = 'i_heading_direction'
               , label = "Heading direction"
               , choices = list("Keep initial direction" = 1, 
                                "Toward next waypoint" = 2)
               , selected = 1
-            )
-            , numericInput(
+            )))
+            , fluidRow(
+                column(width = 6, numericInput(
                 inputId = 'i_grid_offset'
                 , label = 'Shift in grid (m)'
                 , value = 10
                 , min = 1
                 , max = 100
                 #, step = 1
-            )
-            , numericInput(
+            ))
+            , column(width = 6, numericInput(
                 inputId = 'i_maximum_flight_speed'
                 , label = 'Maximum flight speed (m/s)'
-                , value = 10, min = 1, max = 30, step = 1)
+                , value = 10, min = 1, max = 30, step = 1)))
             , numericInput(
                 inputId = 'i_battery_life'
                 , label = 'Battery life (min)'
@@ -62,42 +64,47 @@ ui_configuration <- function() {
                 , label = 'Camara'
                 , choices  = cameras$name
                 , selected = cameras$name[1])
-            , numericInput(
+            , fluidRow(
+                column(width = 6, numericInput(
                 inputId = 'i_shutter_interval'
                 , label = 'Shutter Interval (s)'
-                , value = 2, min = 1, max = 60, step = 1)
-            , numericInput(
-                inputId = 'i_img_sensor_x'
-                , label = 'Image Sensor X (mm)'
-                , value = 17.3, min = 1, max = 100, step = 0.1)
-            , numericInput(
-                inputId = 'i_img_sensor_y'
-                , label = 'Image Sensor Y (mm)'
-                , value = 13.0, min = 1, max = 100, step = 0.1)
-            , numericInput(
+                , value = 2, min = 1, max = 60, step = 1))
+            , column(width = 6, numericInput(
                 inputId = 'i_focus_length'
                 , label = 'Focus Length (mm)'
-                , value = 15, min = 1, max = 100, step = 0.1)
-            , numericInput(
+                , value = 15, min = 1, max = 100, step = 0.1)))
+            , fluidRow(
+                column(width = 6, numericInput(
+                inputId = 'i_img_sensor_x'
+                , label = 'Image Sensor X (mm)'
+                , value = 17.3, min = 1, max = 100, step = 0.1))
+            , column(width = 6, numericInput(
+                inputId = 'i_img_sensor_y'
+                , label = 'Image Sensor Y (mm)'
+                , value = 13.0, min = 1, max = 100, step = 0.1)))
+
+            , fluidRow(
+                column(width = 6, numericInput(
                 inputId = 'i_img_res_x'
                 , label = 'Image Resolution X (pixel)'
-                , value = 2000, min = 100, max = 8000, step = 100)
-            , numericInput(
+                , value = 2000, min = 100, max = 8000, step = 100))
+            , column(width = 6, numericInput(
                 inputId = 'i_img_res_y'
-                , label = 'Image Sensor Y (pixel)'
-                , value = 2000, min = 100, max = 8000, step = 100)
+                , label = 'Image resolution Y (pixel)'
+                , value = 2000, min = 100, max = 8000, step = 100)))
             
-            , selectInput(
+            , fluidRow(
+                column(width = 6, selectInput(
                 inputId = 'i_camera_direction'
                 , label = 'Camera Orientation'
                 , choices = c('Landscape', 'Portrait')
                 , selected = 'Landscape'
-            )
-            , sliderInput(
+            ))
+            , column(width = 6, sliderInput(
                 inputId = 'i_camera_angle'
                 , label = 'Camera angle (degree)'
                 , min = -90, max = 90, value = 0
-            ) 
+            ) ))
         )
         , tabPanel(
             title = 'Field'
@@ -112,14 +119,15 @@ ui_configuration <- function() {
                 , selected = 'fm_grid'
                 , inline = TRUE)
 
-            , sliderInput(
+            , fluidRow(
+                column(width = 6, sliderInput(
                 inputId = 'i_overlap_x'
                 , label = 'Overlap X (%)'
-                , value = 80, min = 0, max = 100)
-            , sliderInput(
+                , value = 80, min = 0, max = 100))
+            , column(width = 6, sliderInput(
                 inputId = 'i_overlap_y'
                 , label = 'Overlap Y (%)'
-                , value = 80, min = 0, max = 100)
+                , value = 80, min = 0, max = 100)))
        )
     )
     
