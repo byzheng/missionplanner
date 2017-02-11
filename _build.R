351c17234fe77f0d0d438b45bc655ab0ac6e0fb3
zhe00a-vc <Bangyou.Zheng@csiro.au>
2017-02-11 12:32:24 +1000

    Remove the travis.yml

--- _build.R
+++
@@ -1,8 +0,0 @@
-args <- commandArgs(TRUE)
-
-token <- args[[1]]
-secret <- args[[2]]
-rmarkdown::render('help.Rmd')
-file.remove('help.html')
-rsconnect::setAccountInfo(name='byzheng', token=token, secret=secret)
-rsconnect::deployApp(quiet=TRUE)
